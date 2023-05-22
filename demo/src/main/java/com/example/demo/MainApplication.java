d4bc3dfe97e64e4aff27c240cf5ec31aaa7d693d
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-05-22 23:17:47 +0300

    created a get request api

---
+++ demo/src/main/java/com/example/demo/MainApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RequestMapping
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello there";
+    }
+}
