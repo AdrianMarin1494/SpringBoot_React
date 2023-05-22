952845716e475be2245b6a4b43b9e5ea44970712
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-05-22 23:30:52 +0300

    added list response

--- demo/src/main/java/com/example/demo/MainApplication.java
+++ demo/src/main/java/com/example/demo/MainApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class MainApplication {
@@ -13,10 +15,11 @@ public class MainApplication {
     }
 
     @GetMapping("/greet")
-    public Dog greet() {
+    public List<Dog> greet() {
         Dog dogTim = new Dog(new Animal("dog", 4), "Tim");
+        Dog dogKim = new Dog(new Animal("dog", 4), "Kim");
 
-        return dogTim;
+        return List.of(dogTim, dogKim);
     }
 
     record Animal(String animalType, int numberOfLegs) {}
