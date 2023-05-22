f63e8bf6ca5e3a35843af0f3be87956be5a2a1fd
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-05-22 23:40:18 +0300

    added array list

--- demo/src/main/java/com/example/demo/MainApplication.java
+++ demo/src/main/java/com/example/demo/MainApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,8 +19,11 @@ public class MainApplication {
     public List<Dog> greet() {
         Dog dogTim = new Dog(new Animal("dog", 4), "Tim");
         Dog dogKim = new Dog(new Animal("dog", 4), "Kim");
+        List<Dog> dogs = new ArrayList<>();
+        dogs.add(dogTim);
+        dogs.add(1, dogKim);
 
-        return List.of(dogTim, dogKim);
+        return dogs;
     }
 
     record Animal(String animalType, int numberOfLegs) {}
